ead1bb0b7f34b8aad309db6d70015ff3a684b6b4
Yosafat <77567907+crypter7054@users.noreply.github.com>
2022-02-13 21:15:24 +0700

    Update dikit

--- poin-a/index.php
+++ poin-a/index.php
@@ -1,21 +1,40 @@
 <?php
 
-include "Mahasiswa.php"
+include "Mahasiswa.php";
 
-// instansi objek mahasiswa dengan memanggil contructor dan setter
+// instansi objek mahasiswa 1 kemudian masukkan data menggunakan setter
 $mahasiswa1 = new Mahasiswa();
 $mahasiswa1->setNIM(2009929);
-$mahasiswa1->setNama(Yosafat);
-$mahasiswa1->setJenisKelamin(Laki-laki);
-$mahasiswa1->setProdi(Ilmu Komputer);
+$mahasiswa1->setNama("Yosafat");
+$mahasiswa1->setJenisKelamin("Laki-laki");
+$mahasiswa1->setProdi("Ilmu Komputer");
 $mahasiswa1->setSemester(4);
 
-// tampilkan objek menggunakan getter
+// tampilkan data mahasiswa 1 menggunakan getter
 echo "Mahasiswa 1". "<br>";
 echo "NIM           : ". $mahasiswa1->getNIM(). "<br>";
 echo "Nama          : ". $mahasiswa1->getNama(). "<br>";
 echo "Jenis Kelamin : ". $mahasiswa1->getJenisKelamin(). "<br>";
 echo "Prodi         : ". $mahasiswa1->getProdi(). "<br>";
 echo "Semester      : ". $mahasiswa1->getSemester(). "<br>";
+
+
+echo "<br>". "**********************************". "<br>". "<br>";
+
+// instansi objek mahasiswa 2 kemudian masukkan data menggunakan setter
+$mahasiswa2 = new Mahasiswa();
+$mahasiswa2->setNIM(2000009);
+$mahasiswa2->setNama("YOS");
+$mahasiswa2->setJenisKelamin("Laki-laki");
+$mahasiswa2->setProdi("Ilmu Komedi");
+$mahasiswa2->setSemester(14);
+
+// tampilkan data mahasiswa 2 menggunakan getter
+echo "Mahasiswa 2". "<br>";
+echo "NIM           : ". $mahasiswa2->getNIM(). "<br>";
+echo "Nama          : ". $mahasiswa2->getNama(). "<br>";
+echo "Jenis Kelamin : ". $mahasiswa2->getJenisKelamin(). "<br>";
+echo "Prodi         : ". $mahasiswa2->getProdi(). "<br>";
+echo "Semester      : ". $mahasiswa2->getSemester(). "<br>";
 
 ?>
